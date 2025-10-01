2bae3baa3eb6607c051e2fadfe3957df4be4b3d2
nati-tewolde <natisnewemail@gmail.com>
2025-10-01 13:45:45 -0400

    completed generated StudentApplication exercise

--- src/main/java/com/pluralsight/Student.java
+++ src/main/java/com/pluralsight/Student.java
@@ -1,4 +1,63 @@
 package com.pluralsight;
 
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private double gpa;
+
+    public Student(){
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.gpa = 0;
+    }
+
+    public Student(String firstName, String lastName, String email, double gpa) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gpa = gpa;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + firstName + " " + lastName +
+                "\nEmail: " + email +
+                "\nGPA: " + gpa;
+    }
+
 }
--- src/main/java/com/pluralsight/StudentApplication.java
+++ src/main/java/com/pluralsight/StudentApplication.java
@@ -1,7 +1,36 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class StudentApplication {
     public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("First name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Last name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Email: ");
+        String email = input.nextLine();
+
+        System.out.print("GPA: ");
+        double gpa = input.nextDouble();
+
+        email = email.trim().toLowerCase();
+
+        Student firstStudent = new Student(firstName, lastName, email, gpa);
+
+        System.out.println(firstStudent);
+
+        if (gpa >= 3.5) {
+            System.out.println("This student is on the honor roll!");
+        } else {
+            System.out.println("This student is in good standing");
+        }
 
     }
 }
